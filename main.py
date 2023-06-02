5624d9bb22f13116640b553448093a78efed829a
ac-playco <110202798+ac-playco@users.noreply.github.com>
2023-06-01 21:00:08 -0400

    V02 serverless

--- main.py
+++ main.py
@@ -30,43 +30,6 @@ from nodes import init_custom_nodes
 import comfy.model_management
 print("LOADING MAIN.PY - V08")
 
-def start_server(args, child_conn, call_on_start=None):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    prompt_server = server.PromptServer(loop, args, child_conn) # Changing here for adding pipe communication
-    q = execution.PromptQueue(prompt_server)
-
-    extra_model_paths_config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "extra_model_paths.yaml")
-    if os.path.isfile(extra_model_paths_config_path):
-        load_extra_path_config(extra_model_paths_config_path)
-
-    if args.extra_model_paths_config:
-        for config_path in itertools.chain(*args.extra_model_paths_config):
-            load_extra_path_config(config_path)
-
-    init_custom_nodes()
-    prompt_server.add_routes() # Changing name for clarity
-    hijack_progress(prompt_server) # Changing name for clarity
-
-    print("Starting prompt_worker thread")
-    threading.Thread(target=prompt_worker, daemon=True, args=(q,prompt_server,)).start() # Changing name for clarity
-
-    if args.output_directory:
-        output_dir = os.path.abspath(args.output_directory)
-        print(f"Setting output directory to: {output_dir}")
-        folder_paths.set_output_directory(output_dir)
-
-    if args.quick_test_for_ci:
-        exit(0)
-
-    try:
-        loop.run_until_complete(run(prompt_server, address=args.listen, port=args.port, verbose=not args.dont_print_server, call_on_start=call_on_start))
-    except Exception as e:
-        print("Error occurred:", e)
-    finally:
-        print("MAIN: Complete Task Loop")
-        #loop.close()
-
 def prompt_worker(q, server):
     e = execution.PromptExecutor(server)
     while True:
@@ -91,7 +54,6 @@ def hijack_progress(server):
         server.send_sync("progress", {"value": value, "max": total}, server.client_id)
         if preview_image_bytes is not None:
             server.send_sync(BinaryEventTypes.PREVIEW_IMAGE, preview_image_bytes, server.client_id)
-            pass
         print(f'MAIN DATA: VALUE:{value} >> MAX: {total}') ## Print Progress
     comfy.utils.set_progress_bar_global_hook(hook)
 
