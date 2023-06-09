d1e9d1c78bb84f672fd523aa405fb5fbbd9b0b6d
ac-pill <110202798+ac-playco@users.noreply.github.com>
2023-06-09 15:46:42 -0400

    Debugging Args

--- comfy/model_management.py
+++ comfy/model_management.py
@@ -2,7 +2,7 @@ import psutil
 from enum import Enum
 from comfy.cli_args import args
 
-print(f'Arguments in model_management:{args.directml}')
+print(f'Arguments in model_management:{args.port}')
 
 class VRAMState(Enum):
     CPU = 0
