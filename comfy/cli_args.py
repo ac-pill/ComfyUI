abe590f75d2b41d745274e0a3b0d710e02d7c358
ac-pill <110202798+ac-playco@users.noreply.github.com>
2023-06-09 12:59:32 -0400

    Still issues with arguments not visible

--- comfy/cli_args.py
+++ comfy/cli_args.py
@@ -54,6 +54,14 @@ def init_args(arg_dict=None):
     args.set_args(arg_dict)
     return args
 
+args = None  # Initialize args to None
+
+def init_args(arg_dict=None):
+    global args
+    args = Arguments()
+    args.set_args(arg_dict)
+    return args
+
 def parse_args(arg_dict=None):
     parser = argparse.ArgumentParser()
 
