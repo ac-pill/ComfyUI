b67e20a4f385783eb009517c8431933b58339222
ac-pill <110202798+ac-playco@users.noreply.github.com>
2023-06-13 17:28:29 -0400

    Adding output path folder

--- server.py
+++ server.py
@@ -495,7 +495,7 @@ class PromptServer():
     def upload_file(self, server_id, port, filenames):
         # Loop over all filenames and upload each file
         output = folder_paths.get_output_directory()
-        print(os. scandir(output))
+        print(os.scandir(output))
         for filename in filenames:
             print(f'FILE NAME: {filename}')
             filepath = os.path.join(output, filename) #need to use output_dir = folder_paths.get_directory_by_type(type)
