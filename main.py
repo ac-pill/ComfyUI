0a404fb2ff813adaae567892606a60cb11e5913c
ac-pill <110202798+ac-playco@users.noreply.github.com>
2023-07-12 17:18:12 -0400

    Fixing issues with Git Conflict merger VS Code errors

--- main.py
+++ main.py
@@ -59,7 +59,53 @@ import execution
 import server
 from server import BinaryEventTypes
 from nodes import init_custom_nodes
+import comfy.model_management
 
+## Start of Edit Block 2 ##
+args = None
+print("LOADING MAIN.PY - V08")
+
+def start_server(child_conn, call_on_start=None):
+    print("now here")
+    global args
+    print(args)
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    prompt_server = server.PromptServer(loop, child_conn) # Changing here for adding pipe communication
+    q = execution.PromptQueue(prompt_server)
+
+    extra_model_paths_config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "extra_model_paths.yaml")
+    if os.path.isfile(extra_model_paths_config_path):
+        load_extra_path_config(extra_model_paths_config_path)
+
+    if args.extra_model_paths_config:
+        for config_path in itertools.chain(*args.extra_model_paths_config):
+            load_extra_path_config(config_path)
+
+    init_custom_nodes()
+    prompt_server.add_routes() # Changing name for clarity
+    hijack_progress(prompt_server) # Changing name for clarity
+
+    print("Starting prompt_worker thread")
+    threading.Thread(target=prompt_worker, daemon=True, args=(q,prompt_server,)).start() # Changing name for clarity
+
+    if args.output_directory:
+        output_dir = os.path.abspath(args.output_directory)
+        print(f"Setting output directory to: {output_dir}")
+        folder_paths.set_output_directory(output_dir)
+
+    if args.quick_test_for_ci:
+        exit(0)
+
+    try:
+        loop.run_until_complete(run(prompt_server, address=args.listen, port=args.port, verbose=not args.dont_print_server, call_on_start=call_on_start))
+    except Exception as e:
+        print("Error occurred:", e)
+    finally:
+        print("MAIN: Complete Task Loop")
+
+## End of Edit Block 2 ##
 
 def prompt_worker(q, server):
     e = execution.PromptExecutor(server)
@@ -69,6 +115,12 @@ def prompt_worker(q, server):
         prompt_id = item[1]
         e.execute(item[2], prompt_id, item[3], item[4])
         q.task_done(item_id, e.outputs_ui)
+        if server.client_id is not None:
+            server.send_sync("executing", { "node": None, "prompt_id": prompt_id }, server.client_id)
+
+        print("Prompt executed in {:.2f} seconds".format(time.perf_counter() - execution_start_time))
+        gc.collect()
+        comfy.model_management.soft_empty_cache()
 
 async def run(server, address='', port=8188, verbose=True, call_on_start=None):
     await asyncio.gather(server.start(address, port, verbose, call_on_start), server.publish_loop())
